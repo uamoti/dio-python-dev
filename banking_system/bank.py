e5c6c67e763ce550a260ffcbc7f15a970d700c8a
uamoti <uamoti@anche.no>
2023-05-09 17:27:22 +0300

    Update to use functions
    Updated to the minimum requirements to use functions. The system would require additional functions to be optimal, but I know we'll have to update to classes so I'll skip it for now.

--- banking_system/bank.py
+++ banking_system/bank.py
@@ -12,7 +12,7 @@ Função depósito: positional only.
 Função extrato: positional and kwargs.
     Positional: saldo
     Kwargs: extrato
-Fique a vontade para adicionar funções a mais, e.g. listar contas.
+Fique a vontade para adicionar funções, e.g. listar contas.
 Armazenar usuários em uma lista.
 O usuário é composto por nome, endereço e CPF.
 Sugestão de endereço: logradouro, número, bairro, cidade e estado.
@@ -49,13 +49,60 @@ def extrato(saldo, /, *, extrato):
     print(extrato)
     print(f'Saldo: R$ {saldo}')
 
-menu = '''\
+def cpf_existe(numero, clientes):
+
+    for cliente in clientes:
+        if cliente['cpf'] == numero:
+            return True
+        else:
+            return False
+
+def cadastrar_cliente(clientes):
+
+    print('---------------------')
+    print('Cadastro de cliente')
+    print('---------------------')
+    cpf = input('CPF do cliente (somente números): ')
+    if cpf_existe(cpf, clientes):
+        print('Cliente existente.')
+        return clientes
+    else:
+        nome = input('Nome do cliente: ')
+        endereco = input('Endereço: ')
+        # cliente = {'cpf': cpf, 'nome': nome, 'endereco': endereco}
+        clientes.append({'cpf': cpf, 'nome': nome, 'endereco': endereco})
+        print('Cliente cadastrado com sucesso!')
+        return clientes
+
+def cadastrar_conta(contas, n_contas, clientes):
+
+    cpf = input('CPF do cliente (somente números): ')
+    if not cpf_existe(cpf, clientes):
+        print('Cliente não existente. Efetue o cadastro primeiro.')
+    else:
+        agencia = '0001'
+        numero = n_contas + 1
+        cliente = get_cliente(cpf, clientes)
+        contas.append({'agencia': agencia, 'número': numero, 'cliente': cliente})
+        print(f'Conta {numero}, agência {agencia}, cadastrada com sucesso!')
+        return contas, numero
+
+def get_cliente(cpf, clientes):
+
+    for cliente in clientes:
+        if cliente['cpf'] == cpf:
+            return cliente
+
+menu = '''
+=========
 Banco DIO
--------------
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+=========
+[ d ] Depositar
+[ s ] Sacar
+[ e ] Extrato
+[ l ] Cadastrar cliente
+[ t ] Cadastrar conta
+[ q ] Sair
 
 => '''
 saldo = 0
@@ -63,6 +110,9 @@ LIMITE_POR_SAQUE = 500
 LIMITE_NUM_SAQUES = 3
 n_saques = 0
 extrato = ''
+clientes = []
+n_contas = 0
+contas = []
 
 while True:
     opcao = input(menu)
@@ -96,9 +146,13 @@ while True:
             limite_saque=LIMITE_POR_SAQUE
         )
     elif opcao == 'e':
-        print(extrato)
+        print('', extrato, sep='\n')
         print(f'Saldo: R$ {saldo}\n')
     elif opcao == 'q':
         break
+    elif opcao == 'l':
+        clientes = cadastrar_cliente(clientes)
+    elif opcao == 't':
+        contas, n_contas = cadastrar_conta(contas, n_contas, clientes)
     else:
         print('Opção inválida\n')
