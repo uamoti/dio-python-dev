93e6bbeece0da9786ba604f8103fb511aa9cafbc
uamoti <uamoti@anche.no>
2023-05-18 18:43:01 +0300

    Update functions from UML and start main function

--- banking_system/oop_bank.py
+++ banking_system/oop_bank.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-from abc import ABC
+from abc import ABC, abstractmethod, abstractproperty
+from datetime import datetime
+import textwrap
 
 
 class Cliente:
@@ -10,10 +12,10 @@ class Cliente:
         self._contas = []
 
     def realizar_transacao(self, conta, transacao):
-        pass
+        transacao.registrar(conta)
 
     def adicionar_conta(self, conta):
-        pass
+        self._contas.append(conta)
 
 class PessoaFisica(Cliente):
 
@@ -25,24 +27,60 @@ class PessoaFisica(Cliente):
 
 class Conta:
 
-    def __init__(self, numero, cliente, historico, saldo=0):
-        self._saldo = saldo
+    def __init__(self, numero, cliente):
+        self._saldo = 0
         self._numero = numero
         self._agencia = '0001'
         self._cliente = cliente
-        self._historico = historico
+        self._historico = Historico()
 
+    @classmethod
+    def nova_conta(cls, cliente, numero):
+        return cls(numero, cliente)
+
+    @property
     def saldo(self):
         return self._saldo
 
-    def nova_conta(self, cliente, numero):
-        pass
+    @property
+    def numero(self):
+        return self._numero
+
+    @property
+    def agencia(self):
+        return self._agencia
+
+    @property
+    def cliente(self):
+        return self._cliente
+
+    @property
+    def historico(self):
+        return self._historico
 
     def sacar(self, valor):
-        pass
+
+        if valor > self._saldo:
+            print("Você não possui saldo suficiente.")
+        elif valor > 0:
+            self._saldo -= valor
+            return True
+        else:
+            print("Valor inválido. Tente novamente.")
+
+        return False
 
     def depositar(self, valor):
-        pass
+
+        if valor < 0:
+            print("Valor inválido. Tente novamente.")
+            return False
+        else:
+            self._saldo += valor
+            return True
+
+    # def __str__(self):
+    #     return f"{self.__class__.__name__}: {', '.join([f'{key}: {value}' for key, value in self.__dict__.items()])}"
 
 class ContaCorrente(Conta):
 
@@ -53,10 +91,24 @@ class ContaCorrente(Conta):
 
 class Historico:
 
+    def __init__(self):
+        self._transacoes = []
+
     def adicionar_transacao(self, transacao):
-        pass
+        self._transacoes.append(
+            {
+                'tipo': transacao.__class__.__name__,
+                'valor': transacao.valor,
+                'data': datetime.now()
+            }
+        )
 
 class Transacao(ABC):
+
+    @property
+    @abstractproperty
+    def valor(self):
+        pass
 
     @abstractmethod
     def registrar(self, conta):
@@ -64,7 +116,57 @@ class Transacao(ABC):
 
 class Deposito(Transacao):
 
-    def registrar(self, conta)
+    def __init__(self, valor):
+        self._valor = valor
+
+    def registrar(self, conta):
+
+        if conta.depositar(self._valor):
+            conta.historico.adicionar_transacao(self)
 
 class Saque(Transacao):
-    pass
+
+    def __init__(self, valor):
+        self._valor = valor
+
+    def registrar(self, conta):
+
+        if conta.sacar(self._valor):
+            conta.historico.adicionar_transacao(self)
+
+def menu():
+
+    menu = """\
+        =========
+        Banco DIO
+        =========
+        [ d ] Depositar
+        [ s ] Sacar
+        [ e ] Extrato
+        [ l ] Cadastrar cliente
+        [ t ] Cadastrar conta
+        [ q ] Sair
+
+        => """
+
+    return input(textwrap.dedent(menu))
+
+def main():
+
+    def sacar():
+        pass
+
+    def depositar():
+        pass
+
+    def criar_cliente():
+        pass
+
+    def criar_conta():
+        pass
+
+    def listar_contas():
+        pass
+
+    def exibir_extrato():
+        pass
