c38522b6902195cd151dfb58f9042ac850638752
uamoti <uamoti@anche.no>
2023-05-20 22:06:55 +0300

    Update functions

--- banking_system/oop_bank.py
+++ banking_system/oop_bank.py
@@ -6,7 +6,6 @@ import textwrap
 
 
 class Cliente:
-
     def __init__(self, endereco):
         self._endereco = endereco
         self._contas = []
@@ -18,16 +17,26 @@ class Cliente:
         self._contas.append(conta)
 
 class PessoaFisica(Cliente):
-
     def __init__(self, cpf, nome, nascimento, endereco):
         self._cpf = cpf
         self._nome = nome
         self._nascimento = nascimento
         super().__init__(endereco)
 
-class Conta:
+    @property
+    def cpf:
+        return self._cpf
 
-    def __init__(self, numero, cliente):
+    @property
+    def nome:
+        return self._nome
+
+    @property
+    def nascimento:
+        return self._nascimento
+
+class Conta:
+    def __init__(self, cliente, numero):
         self._saldo = 0
         self._numero = numero
         self._agencia = '0001'
@@ -59,19 +68,17 @@ class Conta:
         return self._historico
 
     def sacar(self, valor):
-
         if valor > self._saldo:
-            print("Você não possui saldo suficiente.")
+            print("Saldo suficiente.")
+            return False
         elif valor > 0:
             self._saldo -= valor
             return True
         else:
             print("Valor inválido. Tente novamente.")
-
-        return False
+            return False
 
     def depositar(self, valor):
-
         if valor < 0:
             print("Valor inválido. Tente novamente.")
             return False
@@ -79,18 +86,27 @@ class Conta:
             self._saldo += valor
             return True
 
-    # def __str__(self):
-    #     return f"{self.__class__.__name__}: {', '.join([f'{key}: {value}' for key, value in self.__dict__.items()])}"
+    def __str__(self):
+        return f"Agência: {self.agencia}\nNúmero: {self.numero}\nTitular: {self.cliente.nome}"
 
 class ContaCorrente(Conta):
-
-    def __init__(self, limite, max_saques, numero, cliente, saldo):
+    def __init__(self, cliente, numero, limite=500, max_saques=3):
         self._limite = limite
         self._max_saques = max_saques
-        super().__init__(numero, cliente, saldo)
+        super().__init__(numero, cliente)
+
+    def sacar(self, valor):
+        n_saques = len([t for t in self.historico.transacoes if t["tipo"] == Saque.__name__])
+
+        if n_saques >= max_saques:
+            print("Você já atingiu o limite de saques por hoje. Tente novamente amanhã")
+        elif valor > self._limite:
+            print(f"Valor máximo para saques é {self._limite}")
+        else:
+            return super().sacar(valor)
+        return False
 
 class Historico:
-
     def __init__(self):
         self._transacoes = []
 
@@ -104,7 +120,6 @@ class Historico:
         )
 
 class Transacao(ABC):
-
     @property
     @abstractproperty
     def valor(self):
@@ -115,7 +130,6 @@ class Transacao(ABC):
         pass
 
 class Deposito(Transacao):
-
     def __init__(self, valor):
         self._valor = valor
 
@@ -125,7 +139,6 @@ class Deposito(Transacao):
             conta.historico.adicionar_transacao(self)
 
 class Saque(Transacao):
-
     def __init__(self, valor):
         self._valor = valor
 
@@ -134,8 +147,72 @@ class Saque(Transacao):
         if conta.sacar(self._valor):
             conta.historico.adicionar_transacao(self)
 
-def menu():
+def sacar():
+    cpf = input("Digite seu CPF (somente números): ")
+    cliente = consultar_cpf(cpf, clientes)
+    contas_cliente = listar_contas_cliente(contas, cliente)
 
+    if not cliente:
+        print("Cliente não encontrado.")
+    elif not contas_cliente:
+        print("Você não possui contas, cadastre uma primeiro.")
+    else:
+        valor = float(print("Valor do saque: "))
+        transacao = Saque(valor)
+        conta = contas_cliente[0]
+        cliente.realizar_transacao(conta, transacao)
+
+def depositar():
+    cpf = input("Digite seu CPF (somente números): ")
+    cliente = consultar_cpf(cpf, clientes)
+    contas_cliente = listar_contas_cliente(contas, cliente)
+
+    if not cliente:
+        print("Cliente não encontrado.")
+    elif not contas_cliente:
+        print("Você não possui contas, cadastre uma primeiro.")
+    else:
+        valor = float(print("Valor do saque: "))
+        transacao = Deposito(valor)
+        conta = contas_cliente[0]
+        cliente.realizar_transacao(conta, transacao)
+
+def criar_cliente():
+    cpf = input("Digite o CPF do cliente (somente números): ")
+    if consultar_cpf(cpf):
+        print("Cliente já cadastrado.")
+    else:
+        nome = input("Nome completo: ")
+        endereco = input("Endereço: ")
+        nascimento = input("Data de nascimento (dd/mm/aaaa): ")
+        clientes.append(
+            PessoaFisica(cpf, nome, nascimento, endereco)
+        )
+        print("Cliente cadastrado com sucesso!")
+
+def criar_conta():
+    cpf = input("Digite o CPF do cliente (somente números): ")
+    cliente = consultar_cpf(cpf, clientes)
+
+    if not cliente:
+        print("Cliente não cadastrado; realize o cadastro primeiro")
+    else:
+        numero = f"{len(contas) + 1:04}"
+        conta = ContaCorrente.nova_conta(cliente, numero)
+
+def listar_contas_cliente(contas, cliente):
+    cpf = cliente.cpf
+    contas_cliente = [c for c in contas if conta.cliente.cpf == cpf]
+    return contas_cliente if contas_cliente else None
+
+def exibir_extrato():
+    pass
+
+def consultar_cpf(cpf, clientes):
+    cliente = [c for c in clientes if c.cpf == cpf]
+    return cliente[0] if cliente else None
+
+def menu():
     menu = """\
         =========
         Banco DIO
@@ -145,28 +222,35 @@ def menu():
         [ e ] Extrato
         [ l ] Cadastrar cliente
         [ t ] Cadastrar conta
+        [ r ] Listar contas
         [ q ] Sair
 
         => """
-
     return input(textwrap.dedent(menu))
 
 def main():
+    clientes = []
+    contas = []
 
-    def sacar():
-        pass
+    opcao = menu()
 
-    def depositar():
-        pass
+    match opcao:
+        case 'd':
+            pass
+        case 's':
+            pass
+        case 'e':
+            pass
+        case 'l':
+            pass
+        case 't':
+            pass
+        case 'r':
+            pass
+        case 'q':
+            break
+        case _:
+            print("Opção inválida.")
 
-    def criar_cliente():
-        pass
-
-    def criar_conta():
-        pass
-
-    def listar_contas():
-        pass
-
-    def exibir_extrato():
-        pass
+if __name__ == '__main__':
+    main()
