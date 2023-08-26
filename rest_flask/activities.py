cc799ef92f006591de2e96aa1e43a02a28c5de93
uamoti <uamoti@anche.no>
2023-08-26 13:56:00 +0300

    Update activities API
    Updated ORM queries; the instructor version was outdated.
    Added query class to list all activities from a person.

--- rest_flask/activities.py
+++ rest_flask/activities.py
@@ -30,7 +30,7 @@ class Person(Base):
     name = Column(String(40))
     age = Column(Integer)
 
-    def __repr__(self):
+    def __str__(self):
         return f'Person name={self.name}, age={self.age}'
 
     def save(self):
@@ -47,14 +47,14 @@ class Activity(Base):
     __tablename__ = 'activities'
     id_ = Column(Integer, primary_key=True)
     description = Column(String(30))
-    completed = Column(Boolean)
+    completed = Column(Boolean, default=False)
     person_id = Column(Integer, ForeignKey('persons.id_'))
     responsible = relationship('Person')
-    created = Column(Date, default=func.current_date)
+    created = Column(Date, default=func.current_date())
 
-    def __repr__(self):
+    def __str__(self):
         return (f'Activity: description={self.description}, completed={self.completed}, '
-                'responsible={self.responsible}, created={self.created}')
+                'responsible={self.responsible.name}, created={self.created}')
 
     def save(self):
         session.add(self)
@@ -68,11 +68,11 @@ class Activity(Base):
 # Classes for REST operations
 class PersonDb(Resource):
     def get(self, name):
-        #person = Person.query.filter_by(name=name).first()
+        """Retrieve a person by its name"""
         stmt = select(Person).where(Person.name == name)
-        result = session.execute(stmt)
-
-        if len(result) == 0:
+        result = session.execute(stmt).first()
+        
+        if result is None:
             response = {'status': 1, 'message': 'Person not found!'}
         else:
             person = result[0]
@@ -80,33 +80,30 @@ class PersonDb(Resource):
 
         return response
 
-    def post(self):
-        data = request.json
-        person = Person(name=data['name'], age=data['age'])
-        person.save()
-
-        return {'id': person.id_, 'name': person.name, 'age': person.age}
-
     def put(self, name):
-        # You can only alter the age of a person
-        person = Person.query.filter_by(name=name).first()
+        """Update a person's age. The person is retrieved by name."""
         data = request.json
+        stmt = select(Person).where(Person.name == name)
+        result = session.execute(stmt).first()
         
-        if len(person) == 0:
+        if result is None:
             response = {'status': 1, 'message': 'Person not found!'}
         else:
+            person = result[0]
             person.age = data['age']
-            person.save()
             response = {'status': 0, 'name': person.name, 'age': person.age}
 
         return response
 
     def delete(self, name):
-        person = Person.query.filter_by(name=name).first()
+        """Remove a person from the DB (by its name)."""
+        stmt = select(Person).where(Person.name == name)
+        result = session.execute(stmt).first()
 
-        if len(person) == 0:
+        if result is None:
             response = {'status': 1, 'message': 'Person not found!'}
         else:
+            person = result[0]
             person.delete()
             response = {'status': 0, 'message': 'Person successfully deleted!'}
 
@@ -115,12 +112,14 @@ class PersonDb(Resource):
 
 class ActivityDb(Resource):
     def get(self, id_):
-        #activity = Activity.query.filter_by(id_=id_).first()
+        """Retrieve and activity by its ID."""
+        stmt = select(Activity).where(Activity.id_ == id_)
+        result = session.execute(stmt).first()
         
-
-        if len(person) == 0:
+        if result is None:
             response = {'status': 1, 'message': 'Activity not found!'}
         else:
+            activity = result[0]
             response = {
                 'description': activity.description,
                 'completed': activity.completed,
@@ -130,66 +129,145 @@ class ActivityDb(Resource):
 
         return response
 
-    def post(self):
-        data = request.json
-        activity = Activity(**data)
-        activity.save()
-
-        return {
-            'id': activity.id_,
-            'description': activity.description,
-            'completed': activity.completed,
-            'responsible': activity.responsible,
-            'created': activity.created
-        }
-
     def put(self, id_):
-        activity = Activity.query.filter_by(id_=id_).first()
-
-        if len(person) == 0:
+        """Update an activity's 'completed' status. The activity is retrieved by ID-"""
+        activity = session.execute(select(Activity).where(Activity.id_ == id_)).first()
+        
+        if activity is None:
             response = {'status': 1, 'message': 'Activity not found!'}
         else:
+            activity = activity[0]
             activity.completed = not activity.completed
             activity.save()
             response = {
                 'id': activity.id_,
                 'description': activity.description,
                 'completed': activity.completed,
-                'responsible': activity.responsible,
-                'created': activity.created
+                'responsible': activity.responsible.name,
+                'created': str(activity.created)
             }
 
         return response
 
     def delete(self, id_):
-        activity = Activity.query.filter_by(id_=id_).first()
+        """Remove an activity from the DB (by its ID)."""
+        activity = session.execute(select(Activity).where(Activity.id_ == id_)).first()
 
-        if len(person) == 0:
+        if activity is None:
             response = {'status': 1, 'message': 'Activity not found!'}
         else:
+            activity = activity[0]
             activity.delete()
             response = {'status': 0, 'message': 'Activity successfully deleted!'}
             
         return response
 
 
-class ListActivityDb(Resource):
+class ListPersonDb(Resource):
     def get(self):
-        stmt = select(Activity)
-        result = session.execute(stmt)
-
-        response = [
-             {
-                 'description': a.description,
-                 'completed': a.completed,
-                 'responsible': a.responsible,
-                 'created': a.created
-             } for a in result
-        ]
+        """List all registered persons."""
+        result = session.execute(select(Person))
+        response = [{'name': p[0].name, 'age': p[0].age} for p in result]
+        
         if len(response) == 0:
-            return {'message': 'The DB is empty!'}
+            return {'message': 'No one registered yet.'}
         else:
             return response
+
+    def post(self):
+        """Add a person to the DB"""
+        data = request.json
+        person = Person(name=data['name'], age=data['age'])
+        person.save()
+
+        return {'id': person.id_, 'name': person.name, 'age': person.age}
+
+    def put(self, name):
+        """Update a person's age. The person is retrieved by name."""
+        data = request.json
+        stmt = select(Person).where(Person.name == name)
+        result = session.execute(stmt).first()
+        
+        if result is None:
+            response = {'status': 1, 'message': 'Person not found!'}
+        else:
+            person = result[0]
+            person.age = data['age']
+            response = {'status': 0, 'name': person.name, 'age': person.age}
+
+        return response
+
+
+class ListActivityDb(Resource):
+    def get(self):
+        """List all the activities in the DB."""
+        stmt = select(Activity)
+        result = session.execute(stmt)
+        response = [
+             {
+                 'description': a[0].description,
+                 'completed': a[0].completed,
+                 'responsible': a[0].responsible.name,
+                 'created': str(a[0].created)
+             } for a in result
+        ]
+
+        if len(response) == 0:
+            return {'message': 'No registered activity yet.'}
+        else:
+            return response
+            
+    def post(self):
+        """Add an activity to the DB."""
+        data = request.json
+        name = data['responsible']
+        person = session.execute(select(Person).where(Person.name == name)).first()
+
+        if person is None:
+            response = {'status': 1, 'message': 'Responsible person not registered.'}
+        else:
+            desc = data['description']
+            comp = data['completed']
+
+            resp = person[0]
+            #date = data['created']
+            activity = Activity(
+                description=desc,
+                completed=comp,
+                responsible=resp
+            )
+            activity.save()
+            response = {
+                'id': activity.id_,
+                'description': activity.description,
+                'completed': activity.completed,
+                'responsible': activity.responsible.name,
+                'created': str(activity.created)
+            }
+
+        return response
+
+
+class ListActivityPerPerson(Resource):
+    def get(self, name):
+        result = session.execute(select(Person).where(Person.name == name)).first()
+
+        if result is None:
+            return {'status': 1, 'message': 'Person not found.'}
+        else:
+            person = result[0]
+            result = session.execute(select(Activity).where(Activity.responsible == person))
+
+            if result is None:
+                return {'message': 'No activity registered for this person'}
+            else:
+                response = [{
+                    'description': a[0].description,
+                    'completed': a[0].completed,
+                    'created': str(a[0].created)
+                } for a in result]
+
+                return response
             
 engine = create_engine('sqlite:///activities.db', echo=True)
 Base.metadata.create_all(engine)
@@ -197,9 +275,11 @@ session = Session(engine)
 app = Flask(__name__)
 api = Api(app)
 # API routes and classes
-api.add_resource(PersonDb, '/person/<string:name>', '/person/')
+api.add_resource(PersonDb, '/person/<string:name>')
+api.add_resource(ListPersonDb, '/person', '/person/')
 api.add_resource(ActivityDb, '/activity/<int:id_>')
 api.add_resource(ListActivityDb, '/activity', '/activity/')
+api.add_resource(ListActivityPerPerson, '/activity/<string:name>')
 
 if __name__ == '__main__':
     app.run(debug=True)
