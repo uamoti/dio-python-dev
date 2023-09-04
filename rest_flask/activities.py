f013b9695dc80430a8a4c14e9097c30aca725ea1
uamoti <uamoti@anche.no>
2023-09-04 18:54:10 +0300

    Add user endpoint and auth
    Added user authentication with flask-httpauth.
    Added DB and view classes.

--- rest_flask/activities.py
+++ rest_flask/activities.py
@@ -19,9 +19,10 @@ from sqlalchemy import (
 )
 from flask import Flask, request
 from flask_restful import Resource, Api
-
+from flask_httpauth import HTTPBasicAuth
 
 Base = declarative_base()
+auth = HTTPBasicAuth()
 
 # Classes for database tables
 class Person(Base):
@@ -65,6 +66,25 @@ class Activity(Base):
         session.commit()
 
 
+class User(Base):
+    __tablename__ = 'users'
+    id_ = Column(Integer, primary_key=True)
+    username = Column(String(20), unique=True)
+    password = Column(String)
+    is_active = Column(Boolean, default=True)
+
+    def save(self):
+        session.add(self)
+        session.commit()
+
+    def delete(self):
+        session.delete(self)
+        session.commit()
+
+    def __str__(self):
+        return f'Username: {self.username}'
+
+    
 # Classes for REST operations
 class PersonDb(Resource):
     def get(self, name):
@@ -80,8 +100,9 @@ class PersonDb(Resource):
 
         return response
 
+    @auth.login_required    
     def put(self, name):
-        """Update a person's age. The person is retrieved by name."""
+        """Update a person's age. The person is retrieved by name"""
         data = request.json
         stmt = select(Person).where(Person.name == name)
         result = session.execute(stmt).first()
@@ -95,6 +116,7 @@ class PersonDb(Resource):
 
         return response
 
+    @auth.login_required    
     def delete(self, name):
         """Remove a person from the DB (by its name)."""
         stmt = select(Person).where(Person.name == name)
@@ -112,7 +134,7 @@ class PersonDb(Resource):
 
 class ActivityDb(Resource):
     def get(self, id_):
-        """Retrieve and activity by its ID."""
+        """Retrieve and activity by its ID"""
         stmt = select(Activity).where(Activity.id_ == id_)
         result = session.execute(stmt).first()
         
@@ -129,8 +151,9 @@ class ActivityDb(Resource):
 
         return response
 
+    @auth.login_required    
     def put(self, id_):
-        """Update an activity's 'completed' status. The activity is retrieved by ID-"""
+        """Update an activity's 'completed' status"""
         activity = session.execute(select(Activity).where(Activity.id_ == id_)).first()
         
         if activity is None:
@@ -149,8 +172,9 @@ class ActivityDb(Resource):
 
         return response
 
+    @auth.login_required    
     def delete(self, id_):
-        """Remove an activity from the DB (by its ID)."""
+        """Remove an activity from the DB"""
         activity = session.execute(select(Activity).where(Activity.id_ == id_)).first()
 
         if activity is None:
@@ -165,7 +189,7 @@ class ActivityDb(Resource):
 
 class ListPersonDb(Resource):
     def get(self):
-        """List all registered persons."""
+        """List all registered persons"""
         result = session.execute(select(Person))
         response = [{'name': p[0].name, 'age': p[0].age} for p in result]
         
@@ -174,6 +198,7 @@ class ListPersonDb(Resource):
         else:
             return response
 
+    @auth.login_required
     def post(self):
         """Add a person to the DB"""
         data = request.json
@@ -182,8 +207,9 @@ class ListPersonDb(Resource):
 
         return {'id': person.id_, 'name': person.name, 'age': person.age}
 
+    @auth.login_required
     def put(self, name):
-        """Update a person's age. The person is retrieved by name."""
+        """Update a person's age"""
         data = request.json
         stmt = select(Person).where(Person.name == name)
         result = session.execute(stmt).first()
@@ -200,7 +226,7 @@ class ListPersonDb(Resource):
 
 class ListActivityDb(Resource):
     def get(self):
-        """List all the activities in the DB."""
+        """List all the activities in the DB"""
         stmt = select(Activity)
         result = session.execute(stmt)
         response = [
@@ -217,8 +243,9 @@ class ListActivityDb(Resource):
         else:
             return response
             
+    @auth.login_required
     def post(self):
-        """Add an activity to the DB."""
+        """Add an activity to the DB"""
         data = request.json
         name = data['responsible']
         person = session.execute(select(Person).where(Person.name == name)).first()
@@ -265,17 +292,118 @@ class ListActivityPerPerson(Resource):
 
                 return response
             
+
+class UserDb(Resource):
+    def get(self, name):
+        """Retrieve a user by username"""
+        stmt = select(User).where(User.username == name)
+        result = session.execute(stmt).first()
+        
+        if result is None:
+            response = {'status': 1, 'message': 'User not found!'}
+        else:
+            user = result[0]
+            response = {'User': user.username, 'active': user.is_active}
+
+        return response
+
+    @auth.login_required    
+    def put(self, name):
+        """Update a user's 'active' status"""
+        data = request.json
+        stmt = select(User).where(User.username == name)
+        result = session.execute(stmt).first()
+        
+        if result is None:
+            response = {'status': 1, 'message': 'User not found!'}
+        else:
+            user = result[0]
+            user.is_active = not user.is_active
+            response = {'status': 0, 'user': user.username, 'active': user.is_active}
+
+        return response
+
+    @auth.login_required    
+    def delete(self, name):
+        """Remove a user from DB"""
+        stmt = select(User).where(User.username == name)
+        result = session.execute(stmt).first()
+
+        if result is None:
+            response = {'status': 1, 'message': 'User not found!'}
+        else:
+            user = result[0]
+            user.delete()
+            response = {'status': 0, 'message': 'User successfully deleted!'}
+
+        return response
+
+
+class ListUserDb(Resource):
+    def get(self):
+        """List all registered users"""
+        result = session.execute(select(User))
+        response = [{'user': p[0].username, 'active': p[0].is_active} for p in result]
+        
+        if len(response) == 0:
+            return {'message': 'No one registered yet.'}
+        else:
+            return response
+
+    def post(self):
+        """Add a user to the DB"""
+        data = request.json
+        user = User(username=data['username'], password=data['password'])
+        user.save()
+
+        return {'user': user.username, 'active': user.is_active}
+
+    @auth.login_required
+    def put(self, name):
+        """Update a user's active status"""
+        data = request.json
+        stmt = select(User).where(User.username == name)
+        result = session.execute(stmt).first()
+        
+        if result is None:
+            response = {'status': 1, 'message': 'User not found!'}
+        else:
+            user = result[0]
+            user.is_active = not user.is_active
+            response = {'status': 0, 'user': user.username, 'active': user.is_active}
+
+        return response
+
+@auth.verify_password
+def check_auth(user, passwd):
+    if not (user and passwd):
+        return False
+
+    stmt = select(User).where(
+        User.username == user,
+        User.password == passwd
+    )
+    user = session.execute(stmt).first()
+
+    if user is None:
+        return False
+    else:
+        return user[0]
+    
 engine = create_engine('sqlite:///activities.db', echo=True)
 Base.metadata.create_all(engine)
 session = Session(engine)
 app = Flask(__name__)
 api = Api(app)
+
 # API routes and classes
 api.add_resource(PersonDb, '/person/<string:name>')
 api.add_resource(ListPersonDb, '/person', '/person/')
 api.add_resource(ActivityDb, '/activity/<int:id_>')
 api.add_resource(ListActivityDb, '/activity', '/activity/')
 api.add_resource(ListActivityPerPerson, '/activity/<string:name>')
+api.add_resource(UserDb, '/user/<string:name>', '/user/<string:name>/')
+api.add_resource(ListUserDb, '/user', '/user/')
 
 if __name__ == '__main__':
     app.run(debug=True)
