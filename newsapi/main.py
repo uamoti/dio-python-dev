7d78f578709cf5fb36403484fad3c0d230c0b740
uamoti <uamoti@anche.no>
2023-10-18 22:03:17 +0300

    Add html response and template

--- newsapi/main.py
+++ newsapi/main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from newsapi import NewsApiClient
 from pydantic import BaseModel
 # import requests
@@ -11,12 +13,13 @@ with open('newsapi.key') as f:
 
 api = NewsApiClient(API_KEY)
 app = FastAPI()
+templates = Jinja2Templates(directory='templates')
 
-@app.get('/top/{country}')
-def get_country_top_news(country: str) -> list[News]:
+
+@app.get('/top/{country}', response_class=HTMLResponse)
+def get_country_top_news(request: Request, country: str) -> list[News]:
     query = api.get_top_headlines(country=country, language=None)
     articles = query['articles']
-    
-    return [News(title=news['title']) for news in articles]
-
+    news = [News(title=article['title']) for article in articles]
+    return templates.TemplateResponse('index.html', {'request': request, 'news': news})
 
