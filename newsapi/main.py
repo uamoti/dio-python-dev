b7c133e9da395251336f8435aff23aa8a948043f
uamoti <uamoti@anche.no>
2023-10-19 22:01:46 +0300

    Add html for choosing country
    I added a new page with text input for choosing the country.
    I implemented a redirect when clicking the button to choose. I took a while
    but the trouble was the 'action' attribute in the form; it has to be empty.

--- newsapi/main.py
+++ newsapi/main.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI, Request
-from fastapi.responses import HTMLResponse
+from fastapi import FastAPI, Request, Form, status
+from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from newsapi import NewsApiClient
 from pydantic import BaseModel
@@ -16,10 +16,19 @@ app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 
 
-@app.get('/top/{country}', response_class=HTMLResponse)
+@app.post('/top/{country}', response_class=HTMLResponse)
 def get_country_top_news(request: Request, country: str) -> list[News]:
     query = api.get_top_headlines(country=country, language=None)
     articles = query['articles']
     news = [News(title=article['title']) for article in articles]
-    return templates.TemplateResponse('index.html', {'request': request, 'news': news})
+    return templates.TemplateResponse('topnews.html', {'request': request, 'news': news})
+
+@app.get('/', response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse('index.html', {'request': request})
+
+@app.post('/', response_class=RedirectResponse)
+def get_country(request: Request, country: str = Form()):
+    redirect_url = request.url_for('get_country_top_news', country=country)
+    return RedirectResponse(redirect_url)
 
