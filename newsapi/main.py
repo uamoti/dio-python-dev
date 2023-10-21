340fd3695bcc4da0bd9180e142442b15b2dd90e5
uamoti <uamoti@anche.no>
2023-10-22 00:05:08 +0300

    Add button back to start page, fix country list

--- newsapi/main.py
+++ newsapi/main.py
@@ -3,6 +3,7 @@ from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
 from newsapi import NewsApiClient
 from pydantic import BaseModel
+from resources import countries
 # import requests
 
 class News(BaseModel):
@@ -17,7 +18,7 @@ templates = Jinja2Templates(directory='templates')
 
 
 @app.post('/top/{country}', response_class=HTMLResponse)
-def get_country_top_news(request: Request, country: str) -> list[News]:
+def get_country_top_news(request: Request, country: str):
     query = api.get_top_headlines(country=country, language=None)
     articles = query['articles']
     news = [News(title=article['title']) for article in articles]
@@ -25,10 +26,11 @@ def get_country_top_news(request: Request, country: str) -> list[News]:
 
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request):
-    return templates.TemplateResponse('index.html', {'request': request})
+    return templates.TemplateResponse('index.html', {'request': request, 'countries': countries})
 
 @app.post('/', response_class=RedirectResponse)
 def get_country(request: Request, country: str = Form()):
-    redirect_url = request.url_for('get_country_top_news', country=country)
+    country = country.capitalize()
+    redirect_url = request.url_for('get_country_top_news', country=countries[country])
     return RedirectResponse(redirect_url)
 
