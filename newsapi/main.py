1dc5affad213ada54920e79e5df2f6fec37a110f
uamoti <uamoti@anche.no>
2023-10-28 17:17:29 +0300

    Add Mongo integration
    First, the DB is checked for documents related to the requested country.
    If none are found, query the API for it and save to the DB.

--- newsapi/main.py
+++ newsapi/main.py
@@ -1,3 +1,4 @@
+import datetime as dt
 from fastapi import FastAPI, Request, Form, status
 from fastapi.responses import HTMLResponse, RedirectResponse
 from fastapi.templating import Jinja2Templates
@@ -5,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from newsapi import NewsApiClient
 from pydantic import BaseModel, HttpUrl, Field
 from resources import countries
-# import requests
+from pymongo import MongoClient
 
 class News(BaseModel):
     title: str
@@ -15,23 +16,40 @@ class News(BaseModel):
 with open('newsapi.key') as f:
     API_KEY = f.readline().strip()
 
+with open('mongo.uri') as f:
+    MONGO_URI = f.readline().strip()
+
+connection = MongoClient(MONGO_URI)
+db = connection.newsapi
+topnews = db.topnews
 api = NewsApiClient(API_KEY)
 app = FastAPI()
 templates = Jinja2Templates(directory='templates')
 app.mount('/static', StaticFiles(directory='static'), name='static')
 
+# If the collection is empty or the posts are old, then I query the API.
+# Otherwise, retrieve the documents and display.
 @app.post('/top/{country}', response_class=HTMLResponse)
 def get_country_top_news(request: Request, country: str):
-    query = api.get_top_headlines(country=country, language=None)
-    articles = query['articles']
-    news = [
-        News(
-            title=article['title'],
-            url=article['url'],
-            img=article['urlToImage'] if article['urlToImage'] is not None else 'https://placebear.com/1500/200'
-        ) for article in articles
-    ]
-    return templates.TemplateResponse('topnews.html', {'request': request, 'news': news})
+    db_news = topnews.find_one({'country': country})
+    if db_news is None:
+        query = api.get_top_headlines(country=country, language=None)
+        data = query['articles']
+        articles = [
+            {'title': item['title'],
+             'url': item['url'],
+             'img': item['urlToImage'] or 'https://picsum.photos/640/120'}
+           for item in data
+        ]
+        document = {
+            'country': country,
+            'time': dt.datetime.now(),
+            'articles': articles
+        }
+        topnews.insert_one(document)
+    else:
+        articles = db_news['articles']
+    return templates.TemplateResponse('topnews.html', {'request': request, 'news': articles})
 
 @app.get('/', response_class=HTMLResponse)
 def index(request: Request):
